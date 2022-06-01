9d205e7737c538eb80f428dab21217830351a7e1
BugCreators <502461775@qq.com>
2022-06-01 20:51:24 +0800

    fix: banner

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
-import pkg from './package.json';
+// import pkg from './package.json';
 
-const banner = `${pkg.name} v${pkg.version}\n(c) 2019 Smohan<https://smohan.net>.\nReleased under the MIT License.\nhttps://smohan.net/blog/d5uvpu`;
+// const banner = `${pkg.name} v${pkg.version}\n(c) 2019 Smohan<https://smohan.net>.\nReleased under the MIT License.\nhttps://smohan.net/blog/d5uvpu`;
 
 const pathResolve = dir => {
   return path.resolve(__dirname, '.', dir);
@@ -22,7 +22,6 @@ export default defineConfig({
   },
   build: {
     assetsDir: './',
-    banner,
     lib: {
       entry: pathResolve('./src/index.ts'),
       name: 'MoColorPicker',
@@ -31,6 +30,7 @@ export default defineConfig({
     minify: 'terser',
     rollupOptions: {
       output: {
+        // banner,
         assetFileNames: 'mo.color-picker.css',
       },
     },
