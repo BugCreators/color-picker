c100bd4a22365ec58970154f81552a879677f905
BugCreators <502461775@qq.com>
2022-08-25 12:38:26 +0800

    chore: 打包时导出类型声明文件

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import dts from 'vite-plugin-dts';
 // import pkg from './package.json';
 
 // const banner = `${pkg.name} v${pkg.version}\n(c) 2019 Smohan<https://smohan.net>.\nReleased under the MIT License.\nhttps://smohan.net/blog/d5uvpu`;
@@ -16,7 +17,7 @@ export default defineConfig({
       '@plugin': pathResolve('plugin'),
     },
   },
-  plugins: [getBabelOutputPlugin({ allowAllFormats: true, presets: [['@babel/preset-env']] })],
+  plugins: [getBabelOutputPlugin({ allowAllFormats: true, presets: [['@babel/preset-env']] }), dts()],
   server: {
     port: 8081,
   },
